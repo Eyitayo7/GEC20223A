421fbb75968c38c1dbe0146af5fee85ad5a29c81
Eyitayo7 <eyitayo6125@bazeuniversity.edu.ng>
2023-03-24 21:10:49 +0100

    second commit

---
+++ Selection.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Selection {
+	public static int age;
+
+	public static void main(String[] args) {
+		getAge();
+		checkAge();
+		
+	}
+	print static void getAge() {
+	System.out.printIn(" enter your age");
+	
+	Scanner input = new Scanner(System.in);
+	age = input.nextInt();
+	
+}
+	public static void checkAge() {
+		if(age <=40) { 
+		printAge();
+		
+		}
+	}
+	
---
+++ src/syntax.java
@@ -0,0 +1,14 @@
+ 
+public class syntax {
+	String name = "Eyitayo";
+	int age = 20;
+
+	public static void main(String[] args) {
+		 //printMyName(); 
+			
+	}
+	public static void printMyName9() {
+		System.out.println("Eyitayo");
+		
+	}
+}
